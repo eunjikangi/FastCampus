c3bafec867d9b82b08998314d49a67f3fefb54c1
eunji kang <miki2323@naver.com>
2024-12-11 08:12:37 +0900

    study: Part3-03.프로젝트 셋업
    React 컴포넌트 사용방법을 배워보았다.

--- PreAcademy/diary/src/App.jsx
+++ PreAcademy/diary/src/App.jsx
@@ -1,21 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function Test(props) {
+  const style = {
+    color: props.color,
+  };
+
+  return (
+    <div>
+      <h1>안녕 이건 테스트야</h1>
+      <h2 style={style}>
+        {props.title} + {props.eundy}
+      </h2>
+      <a href="https://vite.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+    </div>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const testObject = {
+    title: "2nd Test",
+    eundy: "custuom",
+    color: "red",
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <Test {...testObject} />
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -29,7 +49,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
