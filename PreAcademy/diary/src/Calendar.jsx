c0f9838fa6c956ea62a05236c3f5442b5de2503e
eunji kang <miki2323@naver.com>
2024-12-16 09:01:34 +0900

    feat: component&css 재설계

--- PreAcademy/diary/src/Calendar.jsx
+++ PreAcademy/diary/src/Calendar.jsx
@@ -3,6 +3,17 @@ import styled from "styled-components";
 import { buildCalendarData } from "./buildCalendarData";
 import "../src/Calendar.css";
 
+function Header() {
+  return (
+    <>
+      <div className="headerUI">
+        <Title />
+        <DitectButton />
+      </div>
+    </>
+  );
+}
+
 function Title() {
   let year = "2024";
   let month = "12";
@@ -17,19 +28,9 @@ function Title() {
 function DitectButton() {
   return (
     <div className="nav">
-      <div>왼</div>
-      <div>오늘</div>
-      <div>오</div>
-    </div>
-  );
-}
-
-function InspireSentence() {
-  let sentence = "내면의 목소리를 듣고 싶다면, 일기를 쓰기 시작하라. - 웅디캉";
-
-  return (
-    <div className="maxims">
-      <div>{sentence}</div>
+      <div className="nav-item">왼</div>
+      <div className="nav-item">오늘</div>
+      <div className="nav-item">오</div>
     </div>
   );
 }
@@ -46,7 +47,7 @@ function Calendar() {
     <>
       <div className="card-container">
         {weeks.map((week) => (
-          <div className="columns-7">
+          <div className="day-group">
             {week.map((day, index) => {
               if (day == "0") return <div></div>;
               if (index === 0) {
@@ -56,7 +57,11 @@ function Calendar() {
                   </div>
                 );
               }
-              return <div key={day}>{day}</div>;
+              return (
+                <div key={day} className="day-cell">
+                  {day}
+                </div>
+              );
             })}
           </div>
         ))}
@@ -65,14 +70,21 @@ function Calendar() {
   );
 }
 
+function InspireSentence() {
+  let sentence = "내면의 목소리를 듣고 싶다면, 일기를 쓰기 시작하라. - 웅디캉";
+
+  return (
+    <div className="maxims">
+      <div>{sentence}</div>
+    </div>
+  );
+}
+
 function DiaryUI() {
   return (
     <>
       <div>
-        <div className="columns-2">
-          <Title />
-          <DitectButton />
-        </div>
+        <Header />
         <Calendar />
         <InspireSentence />
       </div>
